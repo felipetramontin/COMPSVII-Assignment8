3defb02f95edef579193ef30a07df1975c61b228
Felipe Tramontin <felipetramontin2008@gmail.com>
2025-10-21 21:03:03 -0400

    Select answers feature added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("script.js connected!");
+const questionBlocks = document.querySelectorAll('.question-block');
+console.log(questionBlocks);
+
+const userAnswers = {};
+
+questionBlocks.forEach((block, index) => {
+  const buttons = block.querySelectorAll('.answer-btn');
+
+  buttons.forEach(btn => {
+    btn.addEventListener('click', () => {
+      buttons.forEach(b => b.classList.remove('selected'));
+
+      btn.classList.add('selected');
+
+      userAnswers[index + 1] = btn.dataset.answer;
+
+      console.log(`Question ${index+1}: ${btn.dataset.answer}`);
+      console.log(userAnswers);
+    });
+  });
+});
